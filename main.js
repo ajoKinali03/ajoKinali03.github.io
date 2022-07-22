ddf9685512973ec3551e3b55669e5d1fa0b5b519
ajoKinali03 <kinaliangek1205@gmail.com>
2022-07-22 09:18:21 +0700

    membuat algoritma slider content

--- main.js
+++ main.js
@@ -30,3 +30,65 @@ pageBtn.addEventListener('click', () => {
     if(biner >= 2){biner *= 0;};
 
 });
+
+
+// cara kerja: yaitu ketika btn knn di tekan maka nilai index akan ditambahkan satu yang akan menjadi value dari array elements dan ketika btn kri di tekan maka nilai akan di kurangi satu dan ketika pada kontent tertentu dot akan berubah panjang nya sesuai urutan dari kontent.
+
+const btnLeft = document.getElementsByClassName('arrow-left')[0];
+const btnRight = document.getElementsByClassName('arrow-right')[0];
+
+
+const countObj = {
+    count: 0,
+};
+
+btnRight.addEventListener('click', () => {
+    const dotStatus = document.getElementsByClassName('dot')[countObj.count];
+
+    if(countObj.count == 0){
+        document.getElementsByClassName('ctn')[0].style.display = 'block';
+        document.getElementsByClassName('ctn')[1].style.display = 'none';
+        document.getElementsByClassName('ctn')[2].style.display = 'none';
+    };
+    if(countObj.count == 1){
+        document.getElementsByClassName('ctn')[0].style.display = 'none';
+        document.getElementsByClassName('ctn')[1].style.display = 'block';
+        document.getElementsByClassName('ctn')[2].style.display = 'none';
+    };
+    if(countObj.count == 2){
+        document.getElementsByClassName('ctn')[0].style.display = 'none';
+        document.getElementsByClassName('ctn')[1].style.display = 'none';
+        document.getElementsByClassName('ctn')[2].style.display = 'block';
+    };
+
+    countObj.count++;
+    if(countObj.count >= 3){
+        countObj.count *= 0;
+    };
+});
+
+btnLeft.addEventListener('click', () => {
+    if(countObj.count >= 0 && countObj.count <= 3){
+        const dotStatus = document.getElementsByClassName('dot')[countObj.count];
+        if(countObj.count == 0){
+            document.getElementsByClassName('ctn')[0].style.display = 'block';
+            document.getElementsByClassName('ctn')[1].style.display = 'none';
+            document.getElementsByClassName('ctn')[2].style.display = 'none';
+        };
+        if(countObj.count == 1){
+            document.getElementsByClassName('ctn')[0].style.display = 'none';
+            document.getElementsByClassName('ctn')[1].style.display = 'block';
+            document.getElementsByClassName('ctn')[2].style.display = 'none';
+        };
+        if(countObj.count == 2){
+            document.getElementsByClassName('ctn')[0].style.display = 'none';
+            document.getElementsByClassName('ctn')[1].style.display = 'none';
+            document.getElementsByClassName('ctn')[2].style.display = 'block';
+        };
+
+        if( countObj.count <= 0){
+            countObj.count += 3;
+        };
+        countObj.count--;
+    };
+});
